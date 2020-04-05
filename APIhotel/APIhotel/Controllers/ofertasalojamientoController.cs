6b094d7cc29d296b109623822ed1bfe668bd7476
victor gonzalez <victorjosegonzalez9@gmail.com>
2020-04-05 16:29:35 -0400

    ofertas added

---
+++ APIhotel/APIhotel/Controllers/ofertasalojamientoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIhotel.Controllers
+{
+    public class ofertasalojamientoController : ApiController
+    {
+        // GET api/ofertasalojamiento
+        public IEnumerable<Models.Hotel.oferta_det_01> Get()
+        {
+            IEnumerable<Models.Hotel.oferta_det_01> rq;
+            using (Models.Hotel.hoteleriaEntities db = new Models.Hotel.hoteleriaEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                rq = db.oferta_det_01.ToList();
+            }
+            return rq;
+        }
+    }
+}
---
+++ APIhotel/APIhotel/Controllers/ofertastusuarioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIhotel.Controllers
+{
+    public class ofertastusuarioController : ApiController
+    {
+        // GET api/ofertas
+        public IEnumerable<Models.Hotel.oferta_det_02> Get()
+        {
+            IEnumerable<Models.Hotel.oferta_det_02> rq;
+            using (Models.Hotel.hoteleriaEntities db = new Models.Hotel.hoteleriaEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                rq = db.oferta_det_02.ToList();
+            }
+            return rq;
+        }
+    }
+}
